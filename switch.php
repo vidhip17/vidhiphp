746786177673c9d0989a438285a51932949e5abd
vidhip17 <77186108+vidhip17@users.noreply.github.com>
2021-01-09 11:51:06 +0530

    switch
    php1

---
+++ switch.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <form method="post" action="switch(1).php">
+            <table>
+                <tr>
+                    <td><input type="text" name="i" placeholder="Enter any number" required/></td>
+                    <td><input type="submit"></td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
+    
