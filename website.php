26b3deae258cc221ff38555f92a0b23693fe6148
vidhip17 <77186108+vidhip17@users.noreply.github.com>
2021-01-09 11:53:58 +0530

    first webpage
    web page

---
+++ website.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+    <h1 align="center">
+        <font size="7" color="white">
+        ONLINE DOCTOR APPOINTMENT SYSTEM
+        </font>    
+    </h1>
+    </head>
+    <body bgcolor="lightblue">
+        <h2 align="left">
+            <font face="lato" size="4" color="white">
+                <a href="https://google.com">HOME</a>&nbsp;&nbsp;
+                <a href="https://www.hospitalsafetygrade.org">HOSPITALS/CLINICS</a>&nbsp;&nbsp;
+                <a href="#">CONSULTATIONS</a>&nbsp;&nbsp;
+                <a href="#">LOGIN/SIGNUP</a>&nbsp;&nbsp;
+            </font>
+            
+        </h2>
+        
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <h3 align="left">
+            <font size="5" color="white">
+            Developed by : VIDHI PATEL
+            </font>
+        </h3>
+    <marquee direction="right">vidhi patel</marquee>
+        
+      
+    </body>
+        
+</html>
